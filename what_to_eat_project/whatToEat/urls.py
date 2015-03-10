91c00830e483eaca9b5d5cbc4a4b6b3df28766d3
unknown <2089278r@BO715-5-01.ad.dcs.gla.ac.uk>
2015-03-10 16:25:00 +0000

    made an about page!

--- what_to_eat_project/whatToEat/urls.py
+++ what_to_eat_project/whatToEat/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from whatToEat import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^about', views.about, name='about'),
+        )
 
 
--- what_to_eat_project/whatToEat/views.py
+++ what_to_eat_project/whatToEat/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("whatTo.Eat is ALIVE!")
 
+def about(request):
+    return HttpResponse("We should have all of our information here")
+
