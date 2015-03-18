cc07d116d7b81d0b3c4f7a6305f7fcdd60da740b
andrewpsuedonym <j.m.walker@live.co.uk>
2015-03-18 12:54:56 +0000

    Population update

--- what_to_eat_project/populate_whatToEat.py
+++ what_to_eat_project/populate_whatToEat.py
@@ -4,32 +4,62 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'what_to_eat_project.settings')
 import django
 django.setup()
 
-from whatToEat.models import Ingredients, Recipe
-
+from whatToEat.models import Ingredient, Recipe, Category, UserProfile
+from django.contrib.auth.models import User
 
 def populate():
+
+    a = []
+    for i in xrange(15):
+        a += [User(username="User "+str(i), password='q')]
+        a[i].save()
+
+    print("Users added but not the profiles")
+    add_profile(User.objects.get(username="User 1"))
 
     add_ingred("Chicken")
     add_ingred("Egg")
     add_ingred("Honey")
     add_ingred("Soy Sauce")
     add_ingred("Ketchup")
+    add_ingred("Milk")
+    add_ingred("Rice")
+    add_ingred("Mushroom")
 
-    ##add_recipe("Fried Egg",
-    ##           5,
-    ##           ingredients = ["Egg"])
+    LunchCat = add_cat("Lunch")
+
+    add_recipe(name="Mushroom omelette", rating=4, author="User 1", category=LunchCat,
+               instructions="\n1) Crack the eggs into a mixing bowl \n2) Add a pinch of salt and pepper"
+                            "\n3) Beat well with a fork Quarter or roughly chop the mushrooms and add to a small frying"
+                            " pan on a high heat with a small knob of butter, a drizzle of olive oil and a pinch of "
+                            "salt and pepper"
+                            "\n4) Fry and toss around until golden, then turn the heat down to medium "
+                            "\n5) Add your eggs and move the pan around to spread them out evenly "
+                            "\n6) When the omelette begins to cook and firm up, but still has a little raw egg on top,"
+                            " sprinkle over the Cheddar, if using "
+                            "\n7) Ease around the edge of the omelette with a spatula, then fold it in half "
+                            "\n8) When it starts to turn golden brown underneath, remove the pan from the heat and "
+                            "slide the omelette on to a plate")
+
+def add_profile(User):
+    u = UserProfile.objects.get_or_create(User=User)[0]
+    return u
 
 
-
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    return c
 
 def add_ingred(name):
-    i = Ingredients.objects.get_or_create(name=name)[0]
+    i = Ingredient.objects.get_or_create(name=name)[0]
     return i
 
-def add_recipe(name, rating, ingredients):
-    recipe, created = Recipe.objects.get_or_create(name=name)
-    recipe.rating = 5
-    ##recipe.ingredients.add(*Ingredients.objects.filter(name__in=[ingredients]))
+
+def add_recipe(name, rating, author, category, instructions):
+    r = Recipe.objects.get_or_create(name=name, rating=rating,
+                                                   author=author, category=category,
+                                                   instructions=instructions)[0]
+    return r
 
 if __name__ == '__main__':
     print "Running population script v0.01"
